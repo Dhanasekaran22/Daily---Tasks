51e92645bfc5ee2db937812480f7ac8665cb4c3d
Dhanasekaran22 <dhanasekaran2622@gmail.com>
2024-10-03 16:44:54 +0530

    object concept updated

--- Typescript/object_concept.js
+++ Typescript/object_concept.js
@@ -6,16 +6,9 @@
 // };
 // console.log(student);
 // console.log(student.fname);
-// let classroom:String[]=["class-1:60","class-2:62",""];
-// console.log(typeof(classroom));                         //in JS array is an object
-// console.log(classroom.length);
-var employeDetails = [
-    { name: "aaa", age: 22, userId: 2622 },
-    { name: "aab", age: 22, userId: 2621 }
-];
-console.log(employeDetails[1].name);
 var studentDetails = [
     { name: "ababc", rollno: "21ucs019", department: "cse" },
     { name: "ababd", rollno: "21ucs020", department: "cse" }
 ];
-console.log(studentDetails[0].rollno);
+console.log(studentDetails[0].gender);
+/*----------------------------------------------------------------------------------------------*/
